8f0b51d6a319a319875aa7b79b46db9c2fcdb3c1
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-03 22:17:45 -0500

    Update module dependencies

--- run/presentation/build.gradle.kts
+++ run/presentation/build.gradle.kts
@@ -36,10 +36,8 @@ dependencies {
     implementation(projects.core.domain)
     implementation(projects.run.domain)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.coil.compose)
+    implementation(libs.google.maps.android.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.timber)
 }
