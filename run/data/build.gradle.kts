20b365b6f9277c59938121dcc9a1ae34ee74ea4e
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-03 22:16:42 -0500

    Add Android Library Convention Plugin

--- run/data/build.gradle.kts
+++ run/data/build.gradle.kts
@@ -1,35 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.runique.android.library)
 }
 
 android {
     namespace = "com.raulastete.run.data"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
@@ -37,11 +11,9 @@ dependencies {
     implementation(projects.core.database)
     implementation(projects.run.domain)
 
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.google.android.gms.play.services.location)
+    implementation(libs.androidx.work)
+    implementation(libs.koin.android.workmanager)
+    implementation(libs.kotlinx.serialization.json)
 }
