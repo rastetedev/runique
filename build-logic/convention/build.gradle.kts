86dd430b7e8bc3f79e3f086352103485aed5b633
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-03 23:32:49 -0500

    Add Android Room Convention Plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -38,5 +38,10 @@ gradlePlugin {
             id = "runique.android.feature.ui"
             implementationClass = "AndroidFeatureUiConventionPlugin"
         }
+
+        register("androidRoom") {
+            id = "runique.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
     }
 }
