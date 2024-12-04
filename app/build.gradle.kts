62375216b77a8e5d1e0d3d67dbf94e897d7904fe
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-03 22:14:44 -0500

    Add Compose plugin to Android Application Compose Convention Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     alias(libs.plugins.runique.android.application.compose)
-
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -32,19 +30,42 @@ dependencies {
     implementation(projects.run.network)
 
 
+    // Coil
+    implementation(libs.coil.compose)
+
+    // Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.navigation.compose)
+
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
 
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
+    // Crypto
+    implementation(libs.androidx.security.crypto.ktx)
 
-    implementation(libs.androidx.material3)
+    api(libs.core)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    // Location
+    implementation(libs.google.android.gms.play.services.location)
+
+    // Splash screen
+    implementation(libs.androidx.core.splashscreen)
+
+    // Timber
+    implementation(libs.timber)
 }
