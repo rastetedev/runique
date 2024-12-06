2cc6979a1e89fdd8f81d05298a93be2ac97baf02
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-06 16:15:04 -0500

    Fix: Update Register Button state

--- auth/presentation/src/main/java/com/raulastete/auth/presentation/register/RegisterState.kt
+++ auth/presentation/src/main/java/com/raulastete/auth/presentation/register/RegisterState.kt
@@ -10,5 +10,6 @@ data class RegisterState(
     val isPasswordVisible: Boolean = false,
     val passwordValidationState: PasswordValidationState = PasswordValidationState(),
     val isRegistering: Boolean = false,
+){
     val canRegister: Boolean = passwordValidationState.isValidPassword && !isRegistering
-)
+}
